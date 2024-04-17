1c450316605159bb6f02fe255acea12b27fba4f2
GioValds <albertovv.con22@utsjr.edu.mx>
2024-04-17 10:31:00 -0600

    cambioooos

--- app/components/Counter.tsx
+++ app/components/Counter.tsx
@@ -1,7 +1,6 @@
 "use client";
 import React, { useState } from 'react';
 
-
 const Counter = () => {
   const [count, setCount] = useState(0);
 
@@ -9,11 +8,16 @@ const Counter = () => {
     setCount(count + 1);
   };
 
+  const reset = () => {
+    setCount(0);
+  };
+
   return (
     <div>
       <h2>Contador</h2>
       <p>El valor actual es: {count}</p>
       <button onClick={increment}>Incrementar</button>
+      <button onClick={reset}>Reiniciar</button> {}
     </div>
   );
 };
